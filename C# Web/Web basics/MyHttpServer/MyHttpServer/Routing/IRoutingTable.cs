1876aa76096fce2836533ec8e148a0c105e8a031
lechev95 <lecho.lechev@gmail.com>
2022-08-28 16:23:38 +0300

    Refactor code

---
+++ C# Web/Web basics/MyHttpServer/MyHttpServer/Routing/IRoutingTable.cs
@@ -0,0 +1,11 @@
+﻿using MyHttpServer.HTTP;
+
+namespace MyHttpServer.Routing
+{
+    public interface IRoutingTable
+    {
+        IRoutingTable Map(string url, Method method, Response response);
+        IRoutingTable MapGet(string url, Response response);
+        IRoutingTable MapPost(string url, Response response);
+    }
+}
