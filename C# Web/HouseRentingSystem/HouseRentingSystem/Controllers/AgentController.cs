393c9f0fce0090c6aeb1b876dc1e28ff0e71af5c
lechev95 <lecho.lechev@gmail.com>
2022-11-18 22:05:11 +0200

    Agent added

--- C# Web/HouseRentingSystem/HouseRentingSystem/Controllers/AgentController.cs
+++ C# Web/HouseRentingSystem/HouseRentingSystem/Controllers/AgentController.cs
@@ -1,4 +1,7 @@
-﻿using HouseRentingSystem.Core.Models.Agent;
+﻿using HouseRentingSystem.Core.Constants;
+using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Core.Models.Agent;
+using HouseRentingSystem.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,15 +10,63 @@ namespace HouseRentingSystem.Controllers
     [Authorize]
     public class AgentController : Controller
     {
-        [HttpGet]
-        public IActionResult Become()
+        private readonly IAgentService agentService;
+
+        public AgentController(IAgentService _agentService)
         {
-            return View();
+            agentService = _agentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Become()
+        {
+            if(await agentService.ExistsByIdAsync(User.Id()))
+            {
+                TempData[MessageConstant.ErrorMessage] = "Вие вече сте агент";
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            var model = new BecomeAgentModel();
+
+            return View(model); 
         }
 
         [HttpPost]
         public async Task<IActionResult> Become(BecomeAgentModel model)
         {
+            var userId = User.Id();
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if(await agentService.ExistsByIdAsync(userId))
+            {
+                TempData[MessageConstant.ErrorMessage] = "Вие вече сте агент";
+
+                return RedirectToAction(actionName: "Index", "Home");
+            }
+
+            if(await agentService.UserWithPhoneNumberExistsAsync(model.PhoneNumber))
+            {
+                TempData[MessageConstant.ErrorMessage] = "Телефона вече съществува";
+
+                return RedirectToAction(actionName: "Index", "Home");
+            }
+
+            if (await agentService.UserHasRentsAsync(userId))
+            {
+                TempData[MessageConstant.ErrorMessage] = "Не трябва да имате наеми, за да станете агент";
+
+                return RedirectToAction(actionName: "Index", "Home");
+            }
+
+            await agentService.Create(userId, model.PhoneNumber);
+
+            TempData[MessageConstant.SuccessMessage] = "Успешно станахте агент";
+
             return RedirectToAction("All", "House");//nameof(HouseController).Replace("Controller", string.Empty));
         }
     }
