a1fff5efb59743e51e2f27758cec0fb8dbe6c0b8
lechev95 <lecho.lechev@gmail.com>
2022-12-15 23:23:26 +0200

    Unit test project added

---
+++ C# Web/LibraryManagementSystem-FinalWebProject/LibraryManagementSystem-FinalWebProject.Core/Models/Statistics/StatisticsServiceModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryManagementSystem_FinalWebProject.Core.Models.Statistics
+{
+    public class StatisticsServiceModel
+    {
+        public int TotalBooks { get; init; }
+        public int TotalRents { get; init; }
+    }
+}
