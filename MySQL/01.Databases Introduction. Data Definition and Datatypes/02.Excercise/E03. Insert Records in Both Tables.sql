31ecb7cb937ae593cf7cb5b3a3260dd14a611498
lechev95 <lecho.lechev@gmail.com>
2024-01-20 18:24:11 +0200

    Create E03. Insert Records in Both Tables.sql

---
+++ MySQL/01.Databases Introduction. Data Definition and Datatypes/02.Excercise/E03. Insert Records in Both Tables.sql
@@ -0,0 +1,11 @@
+USE minions;
+
+INSERT INTO towns VALUE
+(1, 'Sofia'),
+(2, 'Plovdiv'),
+(3, 'Varna');
+
+INSERT INTO minions VALUE
+(1, 'Kevin', 22, 1),
+(2, 'Bob', 15, 3),
+(3, 'Steward', NULL, 2);
