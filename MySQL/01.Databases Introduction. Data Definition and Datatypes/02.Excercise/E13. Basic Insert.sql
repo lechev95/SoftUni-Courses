d124611327703dac8ec7bc75200deaa0ac10babf
lechev95 <lecho.lechev@gmail.com>
2024-01-26 15:11:55 +0200

    Create E13. Basic Insert.sql

---
+++ MySQL/01.Databases Introduction. Data Definition and Datatypes/02.Excercise/E13. Basic Insert.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE IF NOT EXISTS soft_uni;
+
+USE soft_uni;
+
+CREATE TABLE towns(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE addresses(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+address_text VARCHAR(255) NOT NULL,
+town_id INT
+);
+
+ALTER TABLE addresses
+ADD CONSTRAINT fk_town_id
+FOREIGN KEY addresses(town_id)
+REFERENCES towns(id);
+
+CREATE TABLE departments(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE employees(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(255) NOT NULL,
+middle_name VARCHAR (255),
+last_name VARCHAR(255) NOT NULL,
+job_title VARCHAR(255) NOT NULL,
+department_id INT,
+hire_date DATE NOT NULL,
+salary DOUBLE(6,2) NOT NULL,
+address_id INT
+);
+
+ALTER TABLE employees
+ADD CONSTRAINT fk_department_idtownsemployees
+FOREIGN KEY employees(department_id)
+REFERENCES departments(id);
+
+ALTER TABLE employees
+ADD CONSTRAINT fk_address_id
+FOREIGN KEY employees(address_id)
+REFERENCES addresses(id);
+
+INSERT INTO towns(name) VALUES
+("Sofia"),
+("Plovdiv"),
+("Varna"),
+("Burgas");
+
+INSERT INTO departments(name) VALUES
+("Engineering"),
+("Sales"),
+("Marketing"),
+("Software Development"),
+("Quality Assurance");
+
+INSERT INTO employees(first_name, middle_name, last_name, job_title, department_id, hire_date, salary) VALUES
+("Ivan","Ivanov","Ivanov",".NET Developer",4,"2013-02-01",3500.00),
+("Petar","Petrov","Petrov","Senior Engineer",1,"2004-03-02",4000.00),
+("Maria","Petrova","Ivanova","Intern",5,"2016-08-28",525.25),
+("Georgi","Terziev","Ivanov","CEO",2,"2007-12-09",3000.00),
+("Peter","Pan","Pan","Intern",3,"2016-08-28",599.88);
