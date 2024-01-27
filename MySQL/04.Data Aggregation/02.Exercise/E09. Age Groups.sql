0b4881e31a49b672211b36114059c3a7bfc5d733
lechev95 <lecho.lechev@gmail.com>
2024-01-27 20:39:36 +0200

    Create E09. Age Groups.sql

---
+++ MySQL/04.Data Aggregation/02.Exercise/E09. Age Groups.sql
@@ -0,0 +1,27 @@
+USE gringotts;
+
+SELECT age_group
+      , COUNT(*) AS wizard_count
+   FROM (
+            -- Select the age and a case-when column from the 'WizzardDeposits' table
+			SELECT age
+	             , CASE
+		              WHEN age BETWEEN 0 AND 10
+			          THEN '[0-10]'
+			          WHEN age BETWEEN 11 AND 20
+			          THEN '[11-20]'
+			          WHEN age BETWEEN 21 AND 30
+			          THEN '[21-30]'
+			          WHEN age BETWEEN 31 AND 40
+			          THEN '[31-40]'
+			          WHEN age BETWEEN 41 AND 50
+			          THEN '[41-50]'
+			          WHEN age BETWEEN 51 AND 60
+			          THEN '[51-60]'
+			          WHEN age >= 61
+			          THEN '[61+]'
+		           END AS age_group
+              FROM wizzard_deposits
+         ) AS age_group_sub
+GROUP BY age_group
+ORDER BY age_group;
