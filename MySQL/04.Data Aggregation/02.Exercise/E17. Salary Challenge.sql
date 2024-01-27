0e79f1b7f6a2e08d5fe1b36cc5a2f2a986f5ff2b
lechev95 <lecho.lechev@gmail.com>
2024-01-27 21:57:22 +0200

    Create E17. Salary Challenge.sql

---
+++ MySQL/04.Data Aggregation/02.Exercise/E17. Salary Challenge.sql
@@ -0,0 +1,12 @@
+USE soft_uni4;
+
+SELECT e.first_name, e.last_name, e.department_id
+FROM employees AS e
+WHERE e.salary > (
+    SELECT AVG(e2.salary)
+    FROM employees AS e2
+    WHERE e2.department_id = e.department_id
+    GROUP BY e2.department_id
+)
+ORDER BY e.department_id, e.employee_id
+LIMIT 10;
