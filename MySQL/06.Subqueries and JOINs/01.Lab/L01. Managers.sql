5e27253ae87fd456909edf6cd7ba8bc7c3f46671
lechev95 <lecho.lechev@gmail.com>
2024-02-04 18:54:05 +0200

    Create L01. Managers.sql

---
+++ MySQL/06.Subqueries and JOINs/01.Lab/L01. Managers.sql
@@ -0,0 +1,8 @@
+USE soft_uni5;
+
+SELECT e.employee_id, CONCAT(e.first_name, " ", e.last_name) AS full_name, d.department_id, d.name AS department_name
+FROM employees AS e
+RIGHT JOIN departments AS d
+ON d.manager_id = e.employee_id
+ORDER BY e.employee_id
+LIMIT 5;
