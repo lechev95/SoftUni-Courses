25ca33c90a37f6a17b1760a97705e9f776e07f2a
lechev95 <lecho.lechev@gmail.com>
2024-02-05 13:45:46 +0200

    Create E05. Employees Without Project.sql

---
+++ MySQL/06.Subqueries and JOINs/02.Exercise/E05. Employees Without Project.sql
@@ -0,0 +1,9 @@
+USE soft_uni5;
+
+SELECT e.employee_id, e.first_name
+FROM employees AS e
+LEFT JOIN employees_projects AS ep
+ON ep.employee_id = e.employee_id
+WHERE ep.project_id IS NULL
+ORDER BY e.employee_id DESC
+LIMIT 3;
