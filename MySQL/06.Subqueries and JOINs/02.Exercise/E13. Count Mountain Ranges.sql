e12f47c5bb694934bfa985225f6ea12a96cabb42
lechev95 <lecho.lechev@gmail.com>
2024-02-06 16:27:59 +0200

    Update E13. Count Mountain Ranges.sql

--- MySQL/06.Subqueries and JOINs/02.Exercise/E13. Count Mountain Ranges.sql
+++ MySQL/06.Subqueries and JOINs/02.Exercise/E13. Count Mountain Ranges.sql
@@ -3,4 +3,5 @@ USE geography3;
 SELECT country_code, COUNT(mountain_id) AS mountain_range
 FROM mountains_countries
 WHERE country_code IN ("US", "RU", "BG")
-GROUP BY country_code;
+GROUP BY country_code
+ORDER BY mountain_range DESC;
